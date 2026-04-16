73a9c9ed541b0d7f7410b810064957d70362f748
aynoncse <aynoncse@gmail.com>
2026-04-16 21:11:09 +0600

    chore: add necessary migration files and models and create relation ships among them

---
+++ database/migrations/2026_04_16_143520_create_projects_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->index('name');
+            $table->string('owner_email');
+            $table->boolean('is_completed')->default(false);
+            $table->date('completed_on')->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
